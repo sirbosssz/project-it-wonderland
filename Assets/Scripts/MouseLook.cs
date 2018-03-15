c9889f177e2862278f2a8a1ca4751c61520c18c8
Sakaiji <ohmlordbowman@hotmail.com>
2018-03-15 14:41:55 +0700

    Change Camera Code, Add Example Pause
    Pause System will make game time's scale = 0
    disable camera to rotate.

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -18,10 +18,12 @@ public class MouseLook : MonoBehaviour {
 	public float sensitivityHor = 9;
 	public float sensitivityVer = 9;
 
-	public float minimumVer = -45;
-	public float maximumVer =  45;
+	public float minimumVer = -15;
+	public float maximumVer =  15;
 
 	private float _rotationX = 0;
+
+    private bool IsRightHold = true;
 
 	void Start () {
 		Rigidbody body = GetComponent<Rigidbody> ();
@@ -32,25 +34,44 @@ public class MouseLook : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (axes == RotationAxes.MouseX) {
-			// mouse in X direction
-			transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityHor, 0);
-		} else if (axes == RotationAxes.MouseY) {
-			// mouse in Y direction
-			_rotationX -= Input.GetAxis("Mouse Y") * sensitivityVer;
-			_rotationX = Mathf.Clamp (_rotationX, minimumVer, maximumVer);
+        //CheckRightMouse();
+        if(IsRightHold && Time.timeScale > 0) {
+            if (axes == RotationAxes.MouseX)
+            {
+                // mouse in X direction
+                transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityHor, 0);
+            }
+            else if (axes == RotationAxes.MouseY)
+            {
+                // mouse in Y direction
+                _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVer;
+                _rotationX = Mathf.Clamp(_rotationX, minimumVer, maximumVer);
 
-			float rotationY = transform.localEulerAngles.y;
-			transform.localEulerAngles = new Vector3 (_rotationX, rotationY, 0);
-		} else {
-			// both direction
-			_rotationX -= Input.GetAxis("Mouse Y") * sensitivityVer;
-			_rotationX = Mathf.Clamp (_rotationX, minimumVer, maximumVer);
+                float rotationY = transform.localEulerAngles.y;
+                transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+            }
+            else
+            {
+                // both direction
+                _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVer;
+                _rotationX = Mathf.Clamp(_rotationX, minimumVer, maximumVer);
 
-			float delta = Input.GetAxis ("Mouse X") * sensitivityHor;
-			float rotationY = transform.localEulerAngles.y + delta;
+                float delta = Input.GetAxis("Mouse X") * sensitivityHor;
+                float rotationY = transform.localEulerAngles.y + delta;
 
-			transform.localEulerAngles = new Vector3 (_rotationX, rotationY, 0);
-		}
+                transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+            }
+        }
+		
 	}
+
+    void CheckRightMouse() {
+        //To check if right mouse is held.
+        //NOT USE FOR NOW
+        if (Input.GetMouseButtonDown(1))
+            IsRightHold = true;
+        else if (Input.GetMouseButtonUp(1))
+            IsRightHold = false;
+
+    }
 }
